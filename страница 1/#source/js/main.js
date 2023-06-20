20ad38d02b6221c3737c0948abf0dc4404d15e73
Aussehenn <87763200+Aussehenn@users.noreply.github.com>
2023-06-20 08:40:19 +0300

    Add files via upload

---
+++ страница 1/#source/js/main.js
@@ -0,0 +1,181 @@
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+createApp({
+    setup() {
+        const LetterA = ref(false)
+        const LetterB = ref(false)
+        const LetterV = ref(false)
+        const LetterG = ref(false)
+        const LetterGG = ref(false)
+        const LetterD = ref(false)
+        const LetterE = ref(false)
+        const LetterUe = ref(false)
+        const LetterGi = ref(false)
+        const LetterZ = ref(false)
+        const LetterUi = ref(false)
+        const LetterI = ref(false)
+        const LetterYI = ref(false)
+        const LetterU = ref(false)
+        const LetterK = ref(false)
+        const LetterL = ref(false)
+        const LetterM = ref(false)
+        const LetterN = ref(false)
+        const LetterO = ref(false)
+        const LetterPi = ref(false)
+        const LetterR = ref(false)
+        const LetterS = ref(false)
+        const LetterT = ref(false)
+        const LetterY = ref(false)
+        const LetterF = ref(false)
+        const LetterX = ref(false)
+        const LetterC = ref(false)
+        const LetterCh = ref(false)
+        const LetterSh = ref(false)
+        const LetterSch = ref(false)
+        const LetterMzn = ref(false)
+        const LetterYu = ref(false)
+        const LetterYa = ref(false)
+
+
+        function toggle() {
+            LetterFalse (),
+            LetterA.value = !LetterA.value
+        }
+        function toggle_b () {
+            LetterFalse ()
+            LetterB.value = !LetterB.value
+        }
+        function toggle_v () {
+            LetterFalse ()
+            LetterV.value = !LetterV.value
+        }
+        function toggle_g () {
+            LetterFalse ()
+            LetterG.value = !LetterG.value
+        }
+        function toggle_gg () {
+            LetterFalse ()
+            LetterGG.value = !LetterGG.value
+        }
+        function toggle_d () {
+            LetterFalse ()
+            LetterD.value = !LetterD.value
+        }
+        function toggle_e () {
+            LetterFalse ()
+            LetterE.value = !LetterE.value
+        }
+        function toggle_ue () {
+            LetterFalse ()
+            LetterUe.value = !LetterUe.value
+        }
+        function toggle_gi () {
+            LetterFalse ()
+            LetterGi.value = !LetterGi.value
+        }
+        function toggle_z () {
+            LetterFalse ()
+            LetterZ.value = !LetterZ.value
+        }
+        function toggle_ui () {
+            LetterFalse ()
+            LetterUi.value = !LetterUi.value
+        }
+        function toggle_i () {
+            LetterFalse ()
+            LetterI.value = !LetterI.value
+        }
+        function toggle_yi () {
+            LetterFalse ()
+            LetterYI.value = !LetterYI.value
+        }        
+        function toggle_u () {
+            LetterFalse ()
+            LetterU.value = !LetterU.value
+        }
+        function toggle_k () {
+            LetterFalse ()
+            LetterK.value = !LetterK.value
+        }
+        function toggle_l () {
+            LetterFalse ()
+            LetterL.value = !LetterL.value
+        }        
+        function toggle_m () {
+            LetterFalse ()
+            LetterM.value = !LetterM.value
+        }
+        function toggle_n () {
+            LetterFalse ()
+            LetterN.value = !LetterN.value
+        }
+        function toggle_o () {
+            LetterFalse ()
+            LetterO.value = !LetterO.value
+        }
+        function toggle_pi () {
+            LetterFalse ()
+            LetterPi.value = !LetterPi.value
+        }
+        function toggle_r () {
+            LetterFalse ()
+            LetterR.value = !LetterR.value
+        }
+        function toggle_s () {
+            LetterFalse ()
+            LetterS.value = !LetterS.value
+        }
+        function toggle_y () {
+            LetterFalse ()
+            LetterY.value = !LetterY.value
+        }
+        function toggle_f () {
+            LetterFalse ()
+            LetterF.value = !LetterF.value
+        }
+        function toggle_x () {
+            LetterFalse ()
+            LetterX.value = !LetterX.value
+        }
+        function toggle_c () {
+            LetterFalse ()
+            LetterC.value = !LetterC.value
+        }
+        function toggle_ch () {
+            LetterFalse ()
+            LetterCh.value = !LetterCh.value
+        }
+        function toggle_sh () {
+            LetterFalse ()
+            LetterSh.value = !LetterSh.value
+        }
+        function toggle_sch () {
+            LetterFalse ()
+            LetterSch.value = !LetterSch.value
+        }
+        function toggle_mzn () {
+            LetterFalse ()
+            LetterMzn.value = !LetterMzn.value
+        }
+        function toggle_yu () {
+            LetterFalse ()
+            LetterYu.value = !LetterYu.value
+        }
+        function toggle_ya () {
+            LetterFalse ()
+            LetterYa.value = !LetterYa.value
+        }
+        function toggle_t () {
+            LetterFalse ()
+            LetterT.value = !LetterT.value
+        }
+        function LetterFalse () {
+            LetterA.value = false, LetterB.value = false, LetterV.value = false, LetterG.value = false, LetterGG.value = false, LetterD.value = false, LetterE.value = false, LetterUe.value = false, LetterGi.value = false, LetterZ.value = false, LetterUi.value = false, LetterI.value = false, LetterYI.value = false, LetterU.value = false, LetterK.value = false, LetterL.value = false, LetterM.value = false, LetterN.value = false, LetterO.value = false, LetterPi.value = false, LetterR.value = false, LetterS.value = false, LetterT.value = false, LetterY.value = false, LetterF.value = false, LetterX.value = false, LetterC.value = false, LetterCh.value = false, LetterSh.value = false, LetterSch.value = false, LetterMzn.value = false, LetterYu.value = false, LetterYa.value = false
+
+        }
+
+    return {
+        LetterA, LetterB, LetterV, LetterG, LetterGG, LetterD, LetterE, LetterUe, LetterGi, LetterZ, LetterUi, LetterI, LetterYI, LetterU, LetterK, LetterL, LetterM, LetterN, LetterO, LetterPi, LetterR, LetterS, LetterT, LetterY, LetterF, LetterX, LetterC, LetterCh, LetterSh, LetterSch, LetterMzn, LetterYu, LetterYa, LetterFalse, toggle, toggle_b, toggle_v, toggle_g, toggle_gg, toggle_d, toggle_e, toggle_ue, toggle_gi, toggle_z, toggle_ui, toggle_i, toggle_yi, toggle_u, toggle_k, toggle_l, toggle_m, toggle_n, toggle_o, toggle_pi, toggle_r, toggle_s, toggle_t, toggle_y, toggle_f, toggle_x, toggle_c, toggle_ch, toggle_sh, toggle_sch, toggle_mzn, toggle_yu, toggle_ya
+    
+    }
+}
+}).mount('#app')
---
+++ страница 1/js/main.js
@@ -0,0 +1,349 @@
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+createApp({
+    setup() {
+        const LetterA = ref(false)
+        const LetterB = ref(false)
+        const LetterV = ref(false)
+        const LetterG = ref(false)
+        const LetterGG = ref(false)
+        const LetterD = ref(false)
+        const LetterE = ref(false)
+        const LetterUe = ref(false)
+        const LetterGi = ref(false)
+        const LetterZ = ref(false)
+        const LetterUi = ref(false)
+        const LetterI = ref(false)
+        const LetterYI = ref(false)
+        const LetterU = ref(false)
+        const LetterK = ref(false)
+        const LetterL = ref(false)
+        const LetterM = ref(false)
+        const LetterN = ref(false)
+        const LetterO = ref(false)
+        const LetterPi = ref(false)
+        const LetterR = ref(false)
+        const LetterS = ref(false)
+        const LetterT = ref(false)
+        const LetterY = ref(false)
+        const LetterF = ref(false)
+        const LetterX = ref(false)
+        const LetterC = ref(false)
+        const LetterCh = ref(false)
+        const LetterSh = ref(false)
+        const LetterSch = ref(false)
+        const LetterMzn = ref(false)
+        const LetterYu = ref(false)
+        const LetterYa = ref(false)
+
+
+        function playSound() {
+            const audio = new Audio('img/A.ogg');
+            audio.play();
+        }
+        function playSoundB() {
+            const B = new Audio('img/B.ogg');
+            B.play();
+        }
+        function playSoundV() {
+            const V = new Audio('img/V.ogg');
+            V.play();
+        }
+        function playSoundG() {
+            const G = new Audio('img/G.ogg');
+            G.play();
+        }
+        function playSoundGG() {
+            const GG = new Audio('img/GG.ogg');
+            GG.play();
+        }
+        function playSoundD() {
+            const D = new Audio('img/D.ogg');
+            D.play();
+        }
+        function playSoundE() {
+            const E = new Audio('img/E.ogg');
+            E.play();
+        }
+        function playSoundUE() {
+            const UE = new Audio('img/UE.ogg');
+            UE.play();
+        }
+        function playSoundGI() {
+            const GI = new Audio('img/GI.ogg');
+            GI.play();
+        }
+        function playSoundZ() {
+            const Z = new Audio('img/Z.ogg');
+            Z.play();
+        }
+        function playSoundUI() {
+            const UI = new Audio('img/UI.ogg');
+            UI.play();
+        }
+        function playSoundI() {
+            const I = new Audio('img/I.ogg');
+            I.play();
+        }
+        function playSoundYI() {
+            const YI = new Audio('img/YI.ogg');
+            YI.play();
+        }
+        function playSoundU() {
+            const U = new Audio('img/U.ogg');
+            U.play();
+        }
+        function playSoundK() {
+            const K = new Audio('img/K.ogg');
+            K.play();
+        }
+        function playSoundL() {
+            const L = new Audio('img/L.ogg');
+            L.play();
+        }
+        function playSoundM() {
+            const M = new Audio('img/M.ogg');
+            M.play();
+        }
+        function playSoundN() {
+            const N = new Audio('img/N.ogg');
+            N.play();
+        }
+        function playSoundO() {
+            const O = new Audio('img/O.ogg');
+            O.play();
+        }
+        function playSoundPI() {
+            const PI = new Audio('img/PI.ogg');
+            PI.play();
+        }
+        function playSoundR() {
+            const R = new Audio('img/R.ogg');
+            R.play();
+        }
+        function playSoundS() {
+            const S = new Audio('img/S.ogg');
+            S.play();
+        }
+        function playSoundT() {
+            const T = new Audio('img/T.ogg');
+            T.play();
+        } 
+        function playSoundY() {
+            const Y = new Audio('img/Y.ogg');
+            Y.play();
+        }  
+        function playSoundF() {
+            const F = new Audio('img/F.ogg');
+            F.play();
+        } 
+        function playSoundX() {
+            const X = new Audio('img/X.ogg');
+            X.play();
+        } 
+        function playSoundC() {
+            const C = new Audio('img/C.ogg');
+            C.play();
+        } 
+        function playSoundCH() {
+            const CH = new Audio('img/CH.ogg');
+            CH.play();
+        } 
+        function playSoundSH() {
+            const SH = new Audio('img/SH.ogg');
+            SH.play();
+        } 
+        function playSoundSCH() {
+            const SCH = new Audio('img/SCH.ogg');
+            SCH.play();
+        } 
+        function playSoundMZN() {
+            const MZN = new Audio('img/MZN.ogg');
+            MZN.play();
+        } 
+        function playSoundYU() {
+            const YU = new Audio('img/YU.ogg');
+            YU.play();
+        } 
+        function playSoundYA() {
+            const YA = new Audio('img/YA.ogg');
+            YA.play();
+        } 
+        function toggle() {
+            LetterFalse (),
+            LetterA.value = !LetterA.value,
+            playSound()
+        }
+        function toggle_b () {
+            LetterFalse ()
+            LetterB.value = !LetterB.value
+            playSoundB()
+        }
+        function toggle_v () {
+            LetterFalse ()
+            LetterV.value = !LetterV.value
+            playSoundV()
+        }
+        function toggle_g () {
+            LetterFalse ()
+            LetterG.value = !LetterG.value
+            playSoundG()
+        }
+        function toggle_gg () {
+            LetterFalse ()
+            LetterGG.value = !LetterGG.value
+            playSoundGG()
+        }
+        function toggle_d () {
+            LetterFalse ()
+            LetterD.value = !LetterD.value
+            playSoundD()
+        }
+        function toggle_e () {
+            LetterFalse ()
+            LetterE.value = !LetterE.value
+            playSoundE()
+        }
+        function toggle_ue () {
+            LetterFalse ()
+            LetterUe.value = !LetterUe.value
+            playSoundUE()
+        }
+        function toggle_gi () {
+            LetterFalse ()
+            LetterGi.value = !LetterGi.value
+            playSoundGI()
+        }
+        function toggle_z () {
+            LetterFalse ()
+            LetterZ.value = !LetterZ.value
+            playSoundZ()
+        }
+        function toggle_ui () {
+            LetterFalse ()
+            LetterUi.value = !LetterUi.value
+            playSoundUI()
+        }
+        function toggle_i () {
+            LetterFalse ()
+            LetterI.value = !LetterI.value
+            playSoundI()
+        }
+        function toggle_yi () {
+            LetterFalse ()
+            LetterYI.value = !LetterYI.value
+            playSoundYI()
+        }        
+        function toggle_u () {
+            LetterFalse ()
+            LetterU.value = !LetterU.value
+            playSoundU()
+        }
+        function toggle_k () {
+            LetterFalse ()
+            LetterK.value = !LetterK.value
+            playSoundK()
+        }
+        function toggle_l () {
+            LetterFalse ()
+            LetterL.value = !LetterL.value
+            playSoundL()
+        }        
+        function toggle_m () {
+            LetterFalse ()
+            LetterM.value = !LetterM.value
+            playSoundM()
+        }
+        function toggle_n () {
+            LetterFalse ()
+            LetterN.value = !LetterN.value
+            playSoundN()
+        }
+        function toggle_o () {
+            LetterFalse ()
+            LetterO.value = !LetterO.value
+            playSoundO()
+        }
+        function toggle_pi () {
+            LetterFalse ()
+            LetterPi.value = !LetterPi.value
+            playSoundPI()
+        }
+        function toggle_r () {
+            LetterFalse ()
+            LetterR.value = !LetterR.value
+            playSoundR()
+        }
+        function toggle_s () {
+            LetterFalse ()
+            LetterS.value = !LetterS.value
+            playSoundS()
+        }
+        function toggle_y () {
+            LetterFalse ()
+            LetterY.value = !LetterY.value
+            playSoundY()
+        }
+        function toggle_f () {
+            LetterFalse ()
+            LetterF.value = !LetterF.value
+            playSoundF()
+        }
+        function toggle_x () {
+            LetterFalse ()
+            LetterX.value = !LetterX.value
+            playSoundX()
+        }
+        function toggle_c () {
+            LetterFalse ()
+            LetterC.value = !LetterC.value
+            playSoundC()
+        }
+        function toggle_ch () {
+            LetterFalse ()
+            LetterCh.value = !LetterCh.value
+            playSoundCH()
+        }
+        function toggle_sh () {
+            LetterFalse ()
+            LetterSh.value = !LetterSh.value
+            playSoundSH()
+        }
+        function toggle_sch () {
+            LetterFalse ()
+            LetterSch.value = !LetterSch.value
+            playSoundSCH()
+        }
+        function toggle_mzn () {
+            LetterFalse ()
+            LetterMzn.value = !LetterMzn.value
+            playSoundMZN()
+        }
+        function toggle_yu () {
+            LetterFalse ()
+            LetterYu.value = !LetterYu.value
+            playSoundYU()
+        }
+        function toggle_ya () {
+            LetterFalse ()
+            LetterYa.value = !LetterYa.value
+            playSoundYA()
+        }
+        function toggle_t () {
+            LetterFalse ()
+            LetterT.value = !LetterT.value
+            playSoundT()
+        }
+        function LetterFalse () {
+            LetterA.value = false, LetterB.value = false, LetterV.value = false, LetterG.value = false, LetterGG.value = false, LetterD.value = false, LetterE.value = false, LetterUe.value = false, LetterGi.value = false, LetterZ.value = false, LetterUi.value = false, LetterI.value = false, LetterYI.value = false, LetterU.value = false, LetterK.value = false, LetterL.value = false, LetterM.value = false, LetterN.value = false, LetterO.value = false, LetterPi.value = false, LetterR.value = false, LetterS.value = false, LetterT.value = false, LetterY.value = false, LetterF.value = false, LetterX.value = false, LetterC.value = false, LetterCh.value = false, LetterSh.value = false, LetterSch.value = false, LetterMzn.value = false, LetterYu.value = false, LetterYa.value = false
+
+        }
+
+    return {
+        LetterA, LetterB, LetterV, LetterG, LetterGG, LetterD, LetterE, LetterUe, LetterGi, LetterZ, LetterUi, LetterI, LetterYI, LetterU, LetterK, LetterL, LetterM, LetterN, LetterO, LetterPi, LetterR, LetterS, LetterT, LetterY, LetterF, LetterX, LetterC, LetterCh, LetterSh, LetterSch, LetterMzn, LetterYu, LetterYa, LetterFalse, toggle, toggle_b, toggle_v, toggle_g, toggle_gg, toggle_d, toggle_e, toggle_ue, toggle_gi, toggle_z, toggle_ui, toggle_i, toggle_yi, toggle_u, toggle_k, toggle_l, toggle_m, toggle_n, toggle_o, toggle_pi, toggle_r, toggle_s, toggle_t, toggle_y, toggle_f, toggle_x, toggle_c, toggle_ch, toggle_sh, toggle_sch, toggle_mzn, toggle_yu, toggle_ya,
+        playSound, playSoundB, playSoundV, playSoundG, playSoundGG, playSoundD, playSoundE, playSoundUE, playSoundGI,
+        playSoundZ, playSoundUI, playSoundI, playSoundYI, playSoundU, playSoundK, playSoundL, playSoundM,
+        playSoundN, playSoundO, playSoundPI, playSoundR, playSoundS, playSoundY,playSoundT,playSoundF,
+        playSoundX, playSoundC, playSoundCH, playSoundSH,playSoundSCH,playSoundMZN,playSoundYU,playSoundYA,
+    }
+}
+}).mount('#app')
